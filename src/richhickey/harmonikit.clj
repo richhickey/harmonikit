4ebcbfec58ff98b40948b913f672a7f4b13edba4
Rich Hickey <richhickey@gmail.com>
2013-11-09 20:22:25 -0500

    wip

--- src/richhickey/harmonikit.clj
+++ src/richhickey/harmonikit.clj
@@ -17,9 +17,9 @@
       :lfo {:toggle 1.0 :rate 0.1 :fscale 0 :ramp 0.2 :amp-mod 0 :freq-mod 0 :depth 0}
       
       :master-env
-      {:gain {:val 1.0 :fscale 0.0 :ascale 0.0}
+      {:gain {:val 1.0 :fscale 0.15 :ascale 0.0}
        :delay {:val 0.0 :fscale 0.0 :ascale 0.0}
-       :attack {:val 0.5 :fscale 0.0 :ascale 0.0}
+       :attack {:val 0.5 :fscale 0.5 :ascale -0.75}
        :decay {:val 0.5 :fscale 0.0 :ascale 0.0}
        :sustain {:val 0.25 :fscale 0.0 :ascale 0.0}
        :fade {:val 0.5 :fscale 0.0 :ascale 0.0}
@@ -67,34 +67,12 @@
 (def b (buffer nparams))
 
 (buffer-id b)
-;(buffer-set! b 0 0.02)
-;(buffer-set! b 3 1)
-;(buffer-set! b 6 0.5)
-;(buffer-set! b 9 0.5)
-;(buffer-set! b 12 2)
-;(buffer-set! b 15 0.1)
-;(buffer-set! b 18 0.5)
 
 (defn scaler [n pbuf aratio fratio]
   (with-overloaded-ugens
     (* (+ 1.0 (index:kr pbuf n))
        (+ 1.0 (* aratio (index:kr pbuf (+ 1 n))))
        (+ 1.0 (* fratio (index:kr pbuf (+ 2 n)))))))
-
-(defn harm [h freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel]
-  (with-overloaded-ugens
-    (let [base (* 24 h)
-          scale (/ 1.0 h)
-          hr1 (* r1 (scaler (+ base 0) pbuf aratio fratio))
-          hl1 (* l1 (scaler (+ base 3) pbuf aratio fratio))
-          hr2 (* r2 (scaler (+ base 6) pbuf aratio fratio))
-          hl2 (* l2 (scaler (+ base 9) pbuf aratio fratio))
-          hr3 (* r3 (scaler (+ base 12) pbuf aratio fratio))
-          hl3 (* l3 (scaler (+ base 15) pbuf aratio fratio))
-          hrel (* rel (scaler (+ base 18) pbuf aratio fratio))
-          hectl (envelope [0 hl1 hl2 hl3 0] [hr1 hr2 hr3 hrel] :linear 3)
-          henv (env-gen:kr hectl gate)]
-      (* henv scale (sin-osc (* h freq))))))
 
 (defn ugen-deps
   "Returns a set of the deps (arguments) of this ugen that are themselves
@@ -160,10 +138,10 @@
                 (map? level) (reduce-kv (fn [_ k v]
                                           (blit v (offset k)))
                                         nil level)
-                (vector? level) (map-indexed (fn [i v]
-                                               (let [off (nth offset i)]
-                                                 (aset arr off v)))
-                                             level)
+                (vector? level) (dorun (map-indexed (fn [i v]
+                                                      (let [off (nth offset i)]
+                                                        (blit v off)))
+                                                    level))
                 (number? level) (aset-double arr offset level)))]
     (blit patch offsets)
     (buffer-write! buf arr)))
@@ -171,9 +149,9 @@
 (patch->buf patch b)
 
 (defn bget
-  ([bid k] (index:kr bid (-> offsets k)))
-  ([bid k1 k2] (index:kr bid (-> offsets k1 k2)))
-  ([bid k1 k2 k3] (index:kr bid (-> offsets k1 k2 k3))))
+  ([bid k] (index:kr bid (-> offsets (get k))))
+  ([bid k1 k2] (index:kr bid (-> offsets (get k1) (get k2))))
+  ([bid k1 k2 k3] (index:kr bid (-> offsets (get k1) (get k2) (get k3)))))
 
 (defn scaled [aval scale ratio]
   (with-overloaded-ugens
@@ -185,10 +163,42 @@
        (exp (* (bget bid :master-env k :ascale) aratio))
        (exp (* (bget bid :master-env k :fscale) fratio)))))
 
+(defn hscaled [aval bid h k]
+  (with-overloaded-ugens
+    (* aval (pow 2 (lin-lin (bget bid :harmonics k h) -1.0 1.0 -3.0 3.0)))))
+
+(defn harm [h bid freq gate aratio fratio delay attack decay sustain fade release]
+  (with-overloaded-ugens
+    (let [amp (/ (scaled
+                  (scaled (bget bid :harmonics :gain h) (bget bid :harmonics :ascale h) aratio)
+                  (bget bid :harmonics :ascale h) fratio)
+                 (+ 1 h))
+          delay (+ delay (bget bid :harmonics :delay h))
+          attack (hscaled attack bid h :attack)
+          decay (hscaled decay bid h :decay)
+          sustain (hscaled sustain bid h :sustain)
+          fade (hscaled fade bid h :fade)
+          release (hscaled release bid h :release)
+          ectl (envelope [0 0 1.0 sustain 0 0]
+                         [delay attack decay fade release]
+                         [0
+                          (bget bid :master-curves :attack)
+                          (bget bid :master-curves :decay)
+                          (bget bid :master-curves :fade)
+                          (bget bid :master-curves :release)]
+                         4)
+          env (env-gen:kr ectl gate)
+          hfreq (* (+ 1 h) freq)]
+      (* (bget bid :harmonics :toggle h)
+         amp env
+         (sin-osc hfreq)
+         ;;anti-alias
+         (select:kr (< hfreq 20000) [0.0 1.0])))))
+
 (definst harmonikit
   [bid (buffer-id b)
    freq 220
-   amp 0.75
+   amp 0.5
    gate 1.0]
   (let [abase 0.125
         fbase 220
@@ -213,190 +223,33 @@
                         (bget bid :master-curves :fade)
                         (bget bid :master-curves :release)]
                        4)
-              env (env-gen:kr ectl gate amp :action FREE)]
-    (* gain env (sin-osc freq))))
-
-
-(time (definst harmonikit
-        [pbuf (buffer-id b)
-         freq 220
-         amp 0.75
-         gate 1.0
-         gain 1.0
-         freq-lfo 0
-         amp-lfo 0]
-        (let [fbase 220
-              abase 0.25
-              aratio (/ (- amp abase) abase)
-              fratio (/ (- freq fbase) fbase)
-              r1 (scaled 0 pbuf aratio fratio)
-              l1 (* amp (scaled 3 pbuf aratio fratio))
-              r2 (scaled 6 pbuf aratio fratio)
-              l2 (* l1 (scaled 9 pbuf aratio fratio))
-              r3 (scaled 12 pbuf aratio fratio)
-              l3 (* l1 (scaled 15 pbuf aratio fratio))
-              rel (scaled 18 pbuf aratio fratio)
-              ectl (envelope [0 l1 l2 l3 0] [r1 r2 r3 rel] :linear 3)
-              env (env-gen:kr ectl gate amp :action FREE)
-              mixed (* gain
-             (+
-              (harm 1 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 2 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 3 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 4 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 5 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 6 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 7 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 8 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 9 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 10 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 11 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 12 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 13 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 14 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 15 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 16 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 17 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 18 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 19 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 20 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 21 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 22 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 23 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 24 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 25 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 26 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 27 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 28 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 29 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 30 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 31 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 32 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 33 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 34 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 35 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 36 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 37 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 38 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 39 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 40 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 41 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 42 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 43 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 44 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 45 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 46 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 47 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 48 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 49 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 50 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 51 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 52 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 53 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 54 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 55 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 56 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 57 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 58 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 59 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 60 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 61 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 62 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 63 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 64 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ))
-              rez (resonz mixed 750 0.01)]
-          (+ mixed (* 4 rez)))))
-
-(time (definst harmonikit
-        [pbuf 0 ;(buffer-id b)
-         freq 220
-         amp 0.75
-         gate 1.0
-         gain 1.0
-         freq-lfo 0
-         amp-lfo 0]
-        (let [fbase 220
-              abase 0.25
-              aratio (/ (- amp abase) abase)
-              fratio (/ (- freq fbase) fbase)
-              r1 (scaled 0 pbuf aratio fratio)
-              l1 (* amp (scaled 3 pbuf aratio fratio))
-              r2 (scaled 6 pbuf aratio fratio)
-              l2 (* l1 (scaled 9 pbuf aratio fratio))
-              r3 (scaled 12 pbuf aratio fratio)
-              l3 (* l1 (scaled 15 pbuf aratio fratio))
-              rel (scaled 18 pbuf aratio fratio)
-              ectl (envelope [0 l1 l2 l3 0] [r1 r2 r3 rel] :linear 3)
-              env (env-gen:kr ectl gate amp :action FREE)
-              ]
-          (* gain
-             (+
-              (harm 1 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 2 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 3 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 4 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 5 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 6 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 7 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 8 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 9 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 10 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 11 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 12 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 13 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 14 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 15 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 16 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 17 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 18 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 19 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 20 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 21 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 22 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 23 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 24 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 25 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 26 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 27 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 28 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 29 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 30 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 31 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              ;(harm 32 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 33 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 34 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 35 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 36 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 37 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 38 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 39 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 40 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 41 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 42 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 43 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 44 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 45 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 46 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 47 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 48 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 49 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 50 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 51 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 52 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 53 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 54 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 55 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 56 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 57 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 58 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 59 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 60 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 61 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 62 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 63 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              (harm 64 freq gate pbuf aratio fratio r1 l1 r2 l2 r3 l3 rel)
-              )))))
+        env (env-gen:kr ectl gate amp :action FREE)]
+    (* gain
+       (+
+        (harm 0 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 1 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 2 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 3 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 4 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 5 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 6 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 7 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 8 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 9 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 10 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 11 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 12 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 13 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 14 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 15 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 16 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 17 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 18 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 19 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 20 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 21 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 22 bid freq gate aratio fratio delay attack decay sustain fade release)
+        (harm 23 bid freq gate aratio fratio delay attack decay sustain fade release)))))
 
 (harmonikit (buffer-id b) 440)
 (stop)
